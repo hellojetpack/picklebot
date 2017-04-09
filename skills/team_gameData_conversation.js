05c8230e4aeb3305167af0d481abcfac07f0fefb
joepal <joe.palomino50@gmail.com>
2017-04-08 19:06:34 -0600

    Add functionality to the hears call

--- skills/team_gameData_conversation.js
+++ skills/team_gameData_conversation.js
@@ -1,12 +1,16 @@
 module.exports = (controller) => {
   // listens to when users want the avg game time so we can send them the avg game time
-  controller.hears(['avg game time', 'average game time'], 'direct_mention, mention', (bot, message) => {
+  controller.hears(['^what .* avg|average game time'], ['direct_message', 'direct_mention'], (bot, message) => {
     // get the teams data collection that matches the team id sent with the message
     controller.storage.teams.get(message.team, (err, teamData) => {
       if (err) {
-        bot.say('I had trouble receiving that from my database.');
+        bot.say(`I had trouble receiving that from my database: ${err}`);
       }
       bot.startConversation(message, (converr, convo) => {
+        if (converr) {
+          bot.say(`I could not start the conversation because of an error: ${converr}`);
+        }
+
         convo.say('Let me check that');
 
         // runs check for that teams avg time
@@ -18,7 +22,7 @@ module.exports = (controller) => {
         } else {
           convo.say('Found it!');
 
-          convo.say(`Here is your team's avg game time: ${teamData.gameData.avgTime}`);
+          convo.say(`Here is your team's avg game time: ${teamData.gameData.avgTime} mins.`);
 
           // send a response depending if the avg game time
           // is high or low
