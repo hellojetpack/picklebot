c77f1b72616f33c698e7ca870ec3f9f90ffde97d
John Parker <jpwebcast@gmail.com>
2017-04-12 11:35:22 -0600

    fix eslint errors

--- skills/help.js
+++ skills/help.js
@@ -1,33 +1,23 @@
 module.exports = (controller) => {
-  
   controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
-    
-    // bot.reply(message, "`You want help!? Well, here ya go: \n" +
-    //   "*@picklebot `next`* - Adds you to the game order list \n" +
-    //   "*@picklebot `next [time]`* - Adds you to the game order list at a specific time \n " +
-    //   "*@picklebot `game order` OR `games` OR `order` OR `list`* - Shows the current game order \n " +
-    //   "*@picklebot `skip`* - Moves your game down one slot in the game order list : Also notifies the next team to begin \n" +
-    //   "*@picklebot `avg game time` OR `average game time`* - Tells you the average game time duh! \n" +
-    //   "*@picklebot `order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK`")
     bot.reply(message, {
-      "attachments": [
+      attachments: [
         {
-          "fallback": "Here are a few things you can ask me:",
-          "color": "#C1F862",
-          "pretext": "Here are a few things you can ask me:",
-          "title": "Hopefully Helpful Commands",
-          "text": "*`@picklebot next`* - Adds you to the game order list \n" +
-          "*`@picklebot next [time]`* - Adds you to the game order list at a specific time \n " +
-          "*`@picklebot game order` OR `games` OR `order` OR `list`* - Shows the current game order \n " +
-          "*`@picklebot skip`* - Moves your game down one slot in the game order list \n" +
-          "*`@picklebot remove`* - Removes your game from the game order list \n" +
-          "*`@picklebot avg game time` OR `average game time`* - Tells you the average game time duh! \n" +
-          "*`@picklebot order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK",
-          "mrkdwn_in": ["text"],
+          fallback: 'Here are a few things you can ask me:',
+          color: '#C1F862',
+          pretext: 'Here are a few things you can ask me:',
+          title: 'Hopefully Helpful Commands',
+          text: '*`@picklebot next`* - Adds you to the game order list \n' +
+          '*`@picklebot next [time]`* - Adds you to the game order list at a specific time \n' +
+          '*`@picklebot game order` OR `games` OR `order` OR `list`* - Shows the current game order \n ' +
+          '*`@picklebot skip`* - Moves your game down one slot in the game order list \n' +
+          '*`@picklebot remove`* - Removes your game from the game order list \n' +
+          '*`@picklebot avg game time` OR `average game time`* - Tells you the average game time duh! \n' +
+          '*`@picklebot order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK',
+          mrkdwn_in: ['text'],
         },
-      ]
-
-      }
-    )
+      ],
+    },
+    );
   });
-}
+};
