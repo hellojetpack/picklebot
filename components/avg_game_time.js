3d87774881dd6aa07277d7a4cf77af75b657c6fc
joepal <joe.palomino50@gmail.com>
2017-04-12 23:26:03 -0600

    Add methods to iterate num of game played

--- components/avg_game_time.js
+++ components/avg_game_time.js
@@ -1,39 +1,39 @@
 
-  // Will determine the average game time when called
-  const returnAvgGameTime = numOfGames => mins => defaultAvg =>
-                            (numOfGames < 20 ? defaultAvg : mins / numOfGames);
+// Will determine the average game time when called
+const returnAvgGameTime = numOfGames => mins => defaultAvg =>
+                          (numOfGames < 20 ? defaultAvg : mins / numOfGames);
 
-  // Update and returns the new team game count or
-  // returns an error if unsuccessful
-  const addGame = (controller, teamID, cb) => {
-    let returnError = false;
+// returns an error if unsuccessful
+const iterateGameDataValue = (controller, zone, zoneID, prop, addValue, cb) => {
+  let returnError = false;
 
-    // check to see if the game count exsits in the database
-    controller.storage.teams.get(teamID, (err, data) => {
-      const zoneData = data;
+  // check to see if the game count exsits in the database
+  controller.storage[zone].get(zoneID, (err, data) => {
+    const zoneData = data;
 
-      if (err) {
-        returnError = new Error(`Could not access my storage because of an error: ${err}`);
-      } else {
-        if (!zoneData || !zoneData.gameData || !zoneData.gameData.gamesPlayed) {
-          returnError = new Error('Something is wrong with your data');
-        }
-
-        if (!returnError) {
-          // update games played by one
-          zoneData.gameData.gamesPlayed += 1;
-
-          // save the team data back to the database and sets gamesPlayed
-          controller.storage.teams.save(zoneData, (saveErr, saved) => {
-            if (saveErr) {
-              returnError = new Error(`I experienced an error adding your task: ${saveErr}`);
-            } else {
-              cb(returnError, zoneData.gameData.gamesPlayed);
-            }
-          });
-        }
+    // handle errors from database and if we are missing team game data
+    if (err) {
+      returnError = new Error(`Could not access my storage because of an error: ${err}`);
+    } else {
+      if (!zoneData || !zoneData.gameData || !zoneData.gameData[prop]) {
+        returnError = new Error('Something is wrong with your data');
       }
-    });
-    // allow a callback
-    };
-  module.exports = addGame;
+
+      if (!returnError) {
+        // update games played by one
+        zoneData.gameData[prop] += addValue;
+
+        // save the team data back to the database and sets gamesPlayed
+        controller.storage[zone].save(zoneData, (saveErr, saved) => {
+          if (saveErr) {
+            returnError = new Error(`I experienced an error adding your task: ${saveErr}`);
+          } else {
+            // allow a callback to do something with games played
+            cb(returnError, zoneData.gameData[prop]);
+          }
+        });
+      }
+    }
+  });
+};
+module.exports = iterateGameDataValue;
