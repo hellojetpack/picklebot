642b5fa529448f896273bc63f6b747289194b007
John Parker <jpwebcast@gmail.com>
2017-04-14 14:00:38 -0600

    Add 'Done' Button to help message

--- skills/help.js
+++ skills/help.js
@@ -4,19 +4,35 @@ module.exports = (controller) => {
       attachments: [
         {
           fallback: 'Here are a few things you can ask me:',
+          callback_id: '981',
           color: '#C1F862',
           pretext: 'Here are a few things you can ask me:',
           title: 'Hopefully Helpful Commands',
           text: '*`@picklebot next`* - Adds you to the game order list \n' +
           '*`@picklebot next [time]`* - Adds you to the game order list at a specific time \n' +
-          '*`@picklebot game order` OR `games` OR `order` OR `list`* - Shows the current game order \n ' +
+          '*`@picklebot games` OR `order`* - Shows the current game order \n ' +
           '*`@picklebot skip`* - Moves your game down one slot in the game order list \n' +
           '*`@picklebot remove`* - Removes your game from the game order list \n' +
-          '*`@picklebot avg game time` OR `average game time`* - Tells you the average game time duh! \n' +
+          '*`@picklebot avg game time`* - Tells you the average game time duh! \n' +
           '*`@picklebot order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK',
           mrkdwn_in: ['text'],
+          attachment_type: 'default',
+          actions: [
+            {
+              name: 'done',
+              text: 'Done',
+              type: 'button',
+              style: 'primary',
+              value: 'done',
+            },
+          ],
         },
       ],
     });
+  });
+  controller.on('interactive_message_callback', (bot, message) => {
+    if (message.callback_id === '981') {
+      bot.replyInteractive(message, 'You Said you were done...so I closed the help screen');
+    }
   });
 };
