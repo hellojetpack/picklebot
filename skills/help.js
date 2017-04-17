5676281120e23f6fb83e481df5c3cfe2f947caff
John Parker <jpwebcast@gmail.com>
2017-04-17 11:43:50 -0600

    Format Help message to be more readable

--- skills/help.js
+++ skills/help.js
@@ -4,17 +4,17 @@ module.exports = (controller) => {
       attachments: [
         {
           fallback: 'Here are a few things you can ask me:',
-          callback_id: '981',
+          callback_id: `981-${message.user}`,
           color: '#C1F862',
           pretext: 'Here are a few things you can ask me:',
           title: 'Hopefully Helpful Commands',
-          text: '*`@picklebot next`* - Adds you to the game order list \n' +
-          '*`@picklebot next [time]`* - Adds you to the game order list at a specific time \n' +
-          '*`@picklebot games` OR `order`* - Shows the current game order \n ' +
-          '*`@picklebot skip`* - Moves your game down one slot in the game order list \n' +
-          '*`@picklebot remove`* - Removes your game from the game order list \n' +
-          '*`@picklebot avg game time`* - Tells you the average game time duh! \n' +
-          '*`@picklebot order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK',
+          text: '• *`@picklebot next`* - Adds you to the game order list \n\n' +
+          '• *`@picklebot next [time]`* - Adds you to the game order list at a specific time \n\n' +
+          '• *`@picklebot game order` OR `games`* - Shows the current game order \n\n' +
+          '• *`@picklebot skip`* - Moves your game down one slot in the game order list \n\n' +
+          '• *`@picklebot remove`* - Removes your game from the game order list \n\n' +
+          '• *`@picklebot avg game time`* - Tells you the average game time duh! \n\n' +
+          '• *`@picklebot order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK',
           mrkdwn_in: ['text'],
           attachment_type: 'default',
           actions: [
@@ -31,7 +31,7 @@ module.exports = (controller) => {
     });
   });
   controller.on('interactive_message_callback', (bot, message) => {
-    if (message.callback_id === '981') {
+    if (message.callback_id === `981-${message.user}`) {
       bot.replyInteractive(message, 'You Said you were done...so I closed the help screen');
     }
   });
