5854ab6cc5c232329408fedeba83e477e8020c73
John Parker <jpwebcast@gmail.com>
2017-04-17 11:46:51 -0600

    Add Bot Help response with command details (#14)

--- skills/help.js
+++ skills/help.js
@@ -1,21 +1,38 @@
-module.exports = function(controller) {
-
-
-    // This before middleware allows the help command to accept sub-thread names as a parameter
-    // so users can say help to get the default thread, but help <subthread> will automatically
-    // jump to that subthread (if it exists)
-    controller.studio.before('help', function(convo, next) {
-
-        // is there a parameter on the help command?
-        // if so, change topic.
-        if (matches = convo.source_message.text.match(/^help (.*)/i)) {
-            if (convo.hasThread(matches[1])) {
-                convo.gotoThread(matches[1]);
-            }
-        }
-
-        next();
-
+module.exports = (controller) => {
+  controller.hears(['help'], ['direct_message', 'direct_mention', 'mention'], (bot, message) => {
+    bot.reply(message, {
+      attachments: [
+        {
+          fallback: 'Here are a few things you can ask me:',
+          callback_id: `981-${message.user}`,
+          color: '#C1F862',
+          pretext: 'Here are a few things you can ask me:',
+          title: 'Hopefully Helpful Commands',
+          text: '• *`@picklebot next`* - Adds you to the game order list \n\n' +
+          '• *`@picklebot next [time]`* - Adds you to the game order list at a specific time \n\n' +
+          '• *`@picklebot game order` OR `games`* - Shows the current game order \n\n' +
+          '• *`@picklebot skip`* - Moves your game down one slot in the game order list \n\n' +
+          '• *`@picklebot remove`* - Removes your game from the game order list \n\n' +
+          '• *`@picklebot avg game time`* - Tells you the average game time duh! \n\n' +
+          '• *`@picklebot order food`* - You just Ordered pizza for the whole team!  You are now the team favorite!! JK',
+          mrkdwn_in: ['text'],
+          attachment_type: 'default',
+          actions: [
+            {
+              name: 'done',
+              text: 'Done',
+              type: 'button',
+              style: 'primary',
+              value: 'done',
+            },
+          ],
+        },
+      ],
     });
-
-}
+  });
+  controller.on('interactive_message_callback', (bot, message) => {
+    if (message.callback_id === `981-${message.user}`) {
+      bot.replyInteractive(message, 'You Said you were done...so I closed the help screen');
+    }
+  });
+};
