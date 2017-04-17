70fbddf69012837e7471a38f0be48ac6c0675e27
joepal <joe.palomino50@gmail.com>
2017-04-16 21:54:12 -0600

    Add comments for better documentation

--- components/message_formating.js
+++ components/message_formating.js
@@ -1,27 +1,34 @@
+
 const interpolateColor = (colorStart, colorEnd, t) =>
                          Math.round(colorStart + ((colorEnd - colorStart) * t));
 
+// takes an rgb value and converts it to a hex value
 const component2Hex = (c) => {
   const hex = c.toString(16);
+  // add 0 padding if needed
   return hex.length === 1 ? `0${hex}` : hex;
 };
 
+// converts a rgb triple array, interpolates it and returns a hex color string
 const createColor = (startArr, endArr, t) => {
   const rgbArr = startArr.map((elem, index) => interpolateColor(elem, endArr[index], t));
   return rgbArr.map(elem => component2Hex(elem)).join('');
 };
 
+// takes an array of attachments and adds a color to each attachment
+// starts with green and color interpolates to a dark blue
 const addColor = aArr => aArr.map((elem, index, arr) => {
   const value = index / (arr.length - 1);
   const color = { color: createColor([11, 225, 162], [11, 57, 225], value).toUpperCase() };
   return Object.assign({}, elem, color);
 });
 
+// takes a game order game slot and converts it into a slack attachment with two fields
 const formatAttachment = (obj) => {
   const attachment = {};
   const fields = [{ value: obj.createdBy, short: true }];
   const timeField = {};
-  const boldOrNo = obj.scheduled ? 'title' : 'value';
+  const boldOrNo = obj.scheduled ? 'title' : 'value'; // bold scheduled times
   timeField[boldOrNo] = obj.gameTime;
   timeField.short = true;
   fields.push(timeField);
@@ -32,17 +39,22 @@ const formatAttachment = (obj) => {
 
 const formatMessage = (arr) => {
   const gameOrderMessage = {};
+
   let attachments = arr.reduce((acc, curr) => acc.concat(formatAttachment(curr)), []);
   attachments = addColor(attachments);
+
+  // adds field titles to the slack message
+  attachments.unshift({ color: 'fff', fields: [{ title: 'Player', short: true }, { title: 'Game Time', short: true }] });
+
+  // adds the footer to the slack message
   attachments.push({ text: '',
     footer: 'Type "@pickleball next" to get on the list!',
     footer_icon: 'https://platform.slack-edge.com/img/default_application_icon.png',
     color: 'fff' });
-  attachments.unshift({ color: 'fff', fields: [{ title: 'Player', short: true }, { title: 'Game Time', short: true }] });
-
   gameOrderMessage.text = ':point_down: Here is the current game list:';
   gameOrderMessage.attachments = attachments;
 
   return gameOrderMessage;
 };
+
 module.exports = { formatAttachment, formatMessage, addColor };
